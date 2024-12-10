40d3a766923a1e82a742c76d87f097df74b2bb13
mcalex468 <alexmaco468@gmail.com>
2024-12-10 16:27:30 +0100

    commit

--- travelRouter/src/views/DestinationsView.vue
+++ travelRouter/src/views/DestinationsView.vue
@@ -1,39 +1,40 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import GoBack from '@/components/GoBack.vue';
 
 const route = useRoute();
 const destination = ref(null);
 
-onMounted(async () => {
+const cargarDestino = async () => {
   const response = await fetch('/data.json');
   const data = await response.json();
-  destination.value = data.destinations.find(dest => dest.id === parseInt(route.params.id));
+  destination.value = data.destinations.find(destino => destino.id === parseInt(route.params.id));
+};
+
+watch(() => route.params.id, () => {
+  cargarDestino();
 });
+
+cargarDestino();
 </script>
 
 <template>
-  <div v-if="destination" class="destination">
-    <h1>{{ destination.name }}</h1>
-    <img :src="`/images/${destination.image}`" :alt="destination.name" />
-    <p>{{ destination.description }}</p>
+  <div :key="$route.fullPath" class="destination">
+    <GoBack />
+    <h1>{{ destination?.name }}</h1>
+    <img v-if="destination" :src="`/images/${destination.image}`" :alt="destination.name" />
+    <p v-if="destination">{{ destination.description }}</p>
 
     <h2>Experiences</h2>
-    <div class="experiences">
+    <div v-if="destination" class="experiences">
       <router-link
-        v-for="experience in destination.experiences"
-        :key="experience.slug"
-        :to="{ name: 'experience.show', params: { slug: experience.slug, id: destination.id } }"
-      >
+        v-for="experience in destination.experiences" :key="experience.slug" :to="{ name: 'experience.show', params: 
+        { slug: experience.slug, id: destination.id } }">
         <h3>{{ experience.name }}</h3>
         <img :src="`/images/${experience.image}`" :alt="experience.name" />
         <p>{{ experience.description }}</p>
       </router-link>
     </div>
   </div>
-  <p v-else>Cargando datos...</p>
 </template>
-
-<style scoped>
-</style>
-  
--- travelRouter/src/views/ExperienceView.vue
+++ travelRouter/src/views/ExperienceView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
+import GoBack from '@/components/GoBack.vue';
 
 const route = useRoute();
 const experience = ref(null);
@@ -8,16 +9,16 @@ const experience = ref(null);
 onMounted(async () => {
   const response = await fetch('/data.json');
   const data = await response.json();
-
-  const destination = data.destinations.find(dest => dest.id === parseInt(route.params.id));
-
+  const destination = data.destinations.find(destino => destino.id === parseInt(route.params.id));
   if (destination) {
-    experience.value = destination.experiences.find(exp => exp.slug === route.params.slug);
+    experience.value = destination.experiences.find(experiencia => experiencia.slug === route.params.slug);
   }
 });
 </script>
+
 <template>
   <div v-if="experience" class="experience">
+    <GoBack />
     <h1>{{ experience.name }}</h1>
     <img :src="`/images/${experience.image}`" :alt="experience.name" />
     <p>{{ experience.description }}</p>
